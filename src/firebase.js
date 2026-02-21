d652bd7376a0885b38fb151dafb43c7f5664bb0a
olubijohn <olubijohn4live@gmail.com>
2026-02-21 20:25:16 +0100

    feat: Initialize Firebase services and add `.vercel` to gitignore.

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIkV4YKZSxdlXn0gndHGhFmzPySRifygs",
-  authDomain: "ecwa-church-reg.firebaseapp.com",
-  projectId: "ecwa-church-reg",
-  storageBucket: "ecwa-church-reg.firebasestorage.app",
-  messagingSenderId: "491408218674",
-  appId: "1:491408218674:web:839273d7e789a86c2d7a2b",
-  measurementId: "G-HH6QY82HPG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
